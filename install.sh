9f36b4fd689a1875ba39e96595d2f2fccb88b6d2
tokane888 <tokane888@gmail.com>
2020-02-09 08:15:37 +0900

    chromeインストール処理追加

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 
 set -eux
 
+install_chrome() {
+  curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+  apt-get update -y
+  apt-get install -y google-chrome-stable
+}
+
 main() {
   apt purge -y libreoffice-* thunderbird*
 
   # 先にopenssh-serverをinstallすると、特定バージョン依存の関係でエラーになるので先にopenssh-client削除
   apt purge -y openssh-client
   apt install -y openssh-server
+
+  apt-get install -y curl
+  # TODO: ubuntu-software消したらアイコンが残った
+  #       アイコン押下で起動するので何か残ってる
+  install_chrome
 }
 
 main
