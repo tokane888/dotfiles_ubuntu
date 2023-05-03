1a0ba8c499418d055bc944a299509d95dcc5a389
tokane888 <tokane888@gmail.com>
2023-05-03 19:15:50 +0900

    feature: activity watchインストール処理追加

--- install.sh
+++ install.sh
@@ -7,8 +7,15 @@ setup_trivial() {
   sudo update-alternatives --set editor /usr/bin/vim.basic
 }
 
+install_packages() {
+  # localhost:5600でdashboardが見られるが、OS再起動するまでは見えない
+  curl -LO https://github.com/ActivityWatch/activitywatch/releases/download/v0.12.2/activitywatch-v0.12.2-linux-x86_64.deb
+  dpkg -i activitywatch-v0.12.2-linux-x86_64.deb
+}
+
 main() {
   setup_trivial
+  install_packages
 
   echo "dotfiles ubuntu script end successfully."
 }
